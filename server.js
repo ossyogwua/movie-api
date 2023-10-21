36f52d2c6e50f0982e2c55e2e261c79a52f194e1
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-10-21 22:26:38 +0200

    models.js file added

--- server.js
+++ server.js
@@ -1,173 +1,200 @@
+const mongoose = require ('mongoose');
+  const models = require ('./models.js');
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
 const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
   const bodyParser = require('body-parser');
   const uuid = require('uuid');
+  
 
-  const app = express();
+  //const app = express();
+  //app.use(express.json());
+  //app.use(express.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('common'));
   
   app.use(bodyParser.json())
 
-
-let topTenMovies = [
-  {
-    title: "Titanic",
-    director: {
-      name: "James Cameron",
-      age: 65
-    },
-    genre: {
-      name: "adventure"
-    }
-  },
-  {
-    title: "Jurrasic Park 1",
-    director: {
-      name: "Steven Speilburg",
-      age: 75
-    },
-    genre: {
-      name: "adventure"
-    }
-  },
-  {
-    title: "Woody Woodpecker",
-    director: {
-      name: "Alex Zamm",
-      age: 61
-    },
-    genre: {
-      name: "Animated Comedy"
-    }
-  },
-  {
-    title: "The Wolf of Wall Street",
-    director: {
-      name: "Martin Scorsese",
-      age: 80
-    },
-    genre: {
-      name: "Crime"
-    }
-  },
-  {
-    title: "Love at First Sight",
-    director: {
-      name: "Venessa Caswill",
-      age: 36,
-    },
-    genre: {
-      name: "Romance"
-    }
-  },
-  {
-    title: "The Foreigner",
-    director: {
-      name: "Martin Campbell",
-      age: 79
-    },
-    genre: {
-      name: "Action Thriller"
-    }
-  },
-  {
-    title: "Forgotten Love",
-    director: {
-      name: "Michal Gazda ",
-      age: 42,
-    },
-    genre: {
-      name: "Drama"
-    }
-  },{
-    title: "Spy Kids: Armageddon",
-    director: {
-      name: "Robert Rodriguez",
-      age: 55
-    },
-    genre: {
-      name: "Family Comedy"
-    }
-  },
-  {
-    title: "The Machine",
-    director: {
-      name: "Peter Atencio",
-      age: 40
-    },
-    genre: {
-      name: "Action Comedy"
-    }
-  },
-  {
-    title: "Monkey King",
-    director: {
-      name: "Anthony Stacchi",
-      age: 59
-    },
-    genre: {
-      name: "Action, Adventure"
-    }
-  },
+  mongoose.connect('mongodb://localhost:27017/ocDB', { useNewUrlParser: 
+true, useUnifiedTopology: true });
 
 
+app.get('/genres', (req, res) => {
+Movies.find({ 'Genre.Name':'Thriller' })
+  .then((movies) => {
+    // Logic here
+  })
+  .catch((err) => {
+    // Logic here
+  });
+});
+  // returns a json object of all the movies
+  app.get('/movies', (req, res) => {
+     Movies.find()
+  .then((movies) => { 
+  })
+  .catch((err) => {
 
-]
+  });
+});
+  // returns a single Json object of a movie
+  app.get('/movies', (req, res) => {
+  Movies.findOne({ Title: req.params.title})
+  .then((movies) => {
+ })
+ .catch((err) => {
 
-// Gets the list of all movies
-app.get('/movies', (req, res) => { res.send('Successful GET request returning data on all the movies');
+ });
+});
+
+ // returns movies by the directors name
+ app.get("/directors", (req, res) => {
+ Movies.find({ 'Director.Name': req.params.directorsName })
+      .then((movies) => {
+      })
+      .catch((err) => {
+      });
+    });
+
+  //Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('/users/:Username', async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  })
 
 });
 
-
-// Return data about a single movie by title 
-app.get('/movies/:Title'), (req, res) => { res.send('Successful GET request returning data on the single movie');
-
-  
-    };
-
-
-
-// Gets data about a genre by name
-app.get('/movies/genre/:genreName'), (req, res) => {res.send('Successful GET request returning genre of all the movies');
-
-  
-    };
-
-
-
-
-//  Return data about a director (age) by name
-app.get('/movies/directors/:directorName'), (req, res) => {res.send('Successful GET request returning data about all the movie directors');
-
-
-    };
-
-// Adds data for a new users
-app.post('/users', (req, res) => {res.send('Successful POST request adding new users');
-
-
-
-  }
-);
-
-// Allow users to add a movie to their list of favorites.
-app.post('/favoriteMovies', (req, res) => {res.send('Successful POST request allowing users to add their list of favorite movies');
-
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-// Deletes an existing user's account.
-app.delete('/users/:account', (req, res) => {res.send('Successful DELETE request deleting users account');
-
-
+// Delete a user by username
+app.delete('/users/:Username', async (req, res) => {
+  await Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// Update the user's account
-app.put('/users/:name/:password/:email', (req, res) => {res.send('Successful PUT request updating users account');
+// Deletes movies from users favorites
+app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+  $pull: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }) 
 
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 
-});
-
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080');
-});
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });

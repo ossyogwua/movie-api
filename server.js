48dd757c93c281c6196043298645975b5453e07d
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-11-06 13:11:32 +0100

    Info updated

--- server.js
+++ server.js
@@ -60,7 +60,6 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://testsite.com",
   "http://localhost:1234",
-  "https://movie-qxvuf71f4-osinachis-projects.vercel.app/",
 ];
 
 app.use(
